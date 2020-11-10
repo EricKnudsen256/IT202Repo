d15613503ed227bd189be08131ae905864da09b7
Eric T Knudsen <ek256@afsconnect1.njit.edu>
2020-11-09 22:47:28 -0500

    All php pages for PointsHistory table

--- project/partials/nav.php
+++ project/partials/nav.php
@@ -9,6 +9,12 @@ require_once(__DIR__ . "/../lib/helpers.php");
         <li><a href="login.php">Login</a></li>
         <li><a href="register.php">Register</a></li>
     <?php endif; ?>
+    <?php if (has_role("Admin")): ?>
+        <li><a href="test_create_scores.php">Create Scores</a></li>
+        <li><a href="test_list_scores.php">List Scores</a></li>
+	<li><a href="test_create_pointshistory.php">Create Point Change</a></li>
+        <li><a href="test_list_pointshistory.php">List Point Change</a></li>
+    <?php endif; ?>
     <?php if (is_logged_in()): ?>
         <li><a href="profile.php">Profile</a></li>
         <li><a href="logout.php">Logout</a></li>
