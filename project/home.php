426c112b0e9009318c2b114febf4514d15589f67
Eric T Knudsen <ek256@afsconnect1.njit.edu>
2020-12-17 23:11:50 -0500

    Final Commit for milestone 4

--- project/home.php
+++ project/home.php
@@ -12,9 +12,24 @@ if (isset($_SESSION["user"]) && isset($_SESSION["user"]["email"])) {
 <?php
 
     $currentTime = date('Y-m-d H:i:s');
-    $oneWeekAgo = strtotime("-1 month");
+    $oneWeekAgo = date('Y-m-d H:i:s', strtotime("-7 days"));
     $db = getDB();
-    $stmt = $db->prepare("SELECT user_id, sum(points_change) as points from PointsHistory group by user_id");
+    $stmt = $db->prepare("SELECT user_id, sum(points_change) as points from PointsHistory WHERE :oneWeekAgo < created group by user_id order by points DESC LIMIT 10 ");
+    $r = $stmt->execute([":oneWeekAgo"=>$oneWeekAgo
+        ]);
+
+   $resultW = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    $oneWeekAgo = date('Y-m-d H:i:s', strtotime("-1 month"));
+    $db = getDB();
+    $stmt = $db->prepare("SELECT user_id, sum(points_change) as points from PointsHistory WHERE :oneWeekAgo < created group by user_id order by points DESC LIMIT 10 ");
+    $r = $stmt->execute([":oneWeekAgo"=>$oneWeekAgo
+        ]);
+
+   $resultM = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    $db = getDB();
+    $stmt = $db->prepare("SELECT user_id, sum(points_change) as points from PointsHistory group by user_id order by points DESC LIMIT 10 ");
     $r = $stmt->execute([
         ]);
 
@@ -27,19 +42,40 @@ if (isset($_SESSION["user"]) && isset($_SESSION["user"]["email"])) {
     <h3>Weekly Points</h3>
     <div class="row">
     <div class="card-group">
-<?php if($resultA && count($resultA) > 0):?>
-    <?php foreach($resultA as $r):?>
+<?php if($resultW && count($resultW) > 0):?>
+    <?php foreach($resultW as $r):?>
         <div class="col-auto mb-3">
             <div class="card" style="width: 18rem;">
                 <div class="card-body">
-                    <div class="card-title">
-                        <?php safer_echo("Weekly Scoreboard");?>
-                    </div>
                     <div class="card-text">
                         <div>User: <?php safer_echo($r["user_id"]);?>
 			</div>
 			<div>Points:  <?php safer_echo($r["points"]);?>
 			</div>
+			</br>
+                </div>
+            </div>
+        </div>
+    <?php endforeach;?>
+<?php endif;?>
+    </div>
+    </div>
+
+    <div class="container-fluid">
+    <h3>Monthly Points</h3>
+    <div class="row">
+    <div class="card-group">
+<?php if($resultM && count($resultM) > 0):?>
+    <?php foreach($resultM as $r):?>
+        <div class="col-auto mb-3">
+            <div class="card" style="width: 18rem;">
+                <div class="card-body">
+                    <div class="card-text">
+                        <div>User: <?php safer_echo($r["user_id"]);?>
+                        </div>
+                        <div>Points:  <?php safer_echo($r["points"]);?>
+                        </div>
+                        </br>
                 </div>
             </div>
         </div>
@@ -49,5 +85,28 @@ if (isset($_SESSION["user"]) && isset($_SESSION["user"]["email"])) {
     </div>
 
 
+
+    <div class="container-fluid">
+    <h3>All Time Points</h3>
+    <div class="row">
+    <div class="card-group">
+<?php if($resultA && count($resultA) > 0):?>
+    <?php foreach($resultA as $r):?>
+        <div class="col-auto mb-3">
+            <div class="card" style="width: 18rem;">
+                <div class="card-body">
+                    <div class="card-text">
+                        <div>User: <?php safer_echo($r["user_id"]);?>
+                        </div>
+                        <div>Points:  <?php safer_echo($r["points"]);?>
+                        </div>
+                        </br>
+                </div>
+            </div>
+        </div>
+    <?php endforeach;?>
+<?php endif;?>
+    </div>
+    </div>
 
 <?php require(__DIR__ . "/partials/flash.php");?>
